f586b995e962c413e85d9dc44d1d2335a0cec8d4
mimant <mimant10@gmail.com>
2024-09-09 01:00:02 +0530

    added cloud function

--- environments/dev/resource/main.tf
+++ environments/dev/resource/main.tf
@@ -155,7 +155,7 @@ resource "google_cloudfunctions2_function" "cloudfunctions" {
         available_memory = try(each.value.service_config.available_memory, null)
         timeout_seconds = try(each.value.service_config.timeout_seconds, null)
         service_account_email = try(each.value.service_config.service_account_email, null)
-		project_id = try(each.value.service_config.environment_variables.project_id, null)
+		environment_variables.project_id = try(each.value.service_config.environment_variables.project_id, null)
 		 }
        }
 
